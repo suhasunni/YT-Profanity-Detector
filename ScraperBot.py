d1f2aedca2832d47d5ec05e939ebcbfa3c5701a2
Suhas Unni <suhasunni6@gmail.com>
2024-08-23 00:07:40 -0400

    Completed transcript collection + text cleaning

--- ScraperBot.py
+++ ScraperBot.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
 import time
+import re
 
 load_dotenv()
 
@@ -23,19 +24,53 @@ class ScraperBot:
 
     def scrapeTranscript(self):
         #click 'more' button to expand description box
-        more_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//tp-yt-paper-button[@id="expand"]')))
+        more_btn = WebDriverWait(self.driver, 100).until(EC.presence_of_element_located((By.XPATH, '//tp-yt-paper-button[@id="expand"]')))
         more_btn.click()
         
         #click 'show transcript' button
-        show_transcript_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[@aria-label="Show transcript"]')))
-        show_transcript_btn.click()
-        print('The button worked!', show_transcript_btn.text)
-        time.sleep(10)
+        try:
+            show_transcript_btn = WebDriverWait(self.driver, 100).until(EC.presence_of_element_located((By.XPATH, '//button[@aria-label="Show transcript"]')))
+            show_transcript_btn.click()
+            print('Transcript Button Found!')
+            
+        except:
+            return False
+        
+        #record transcript
+        transcript = WebDriverWait(self.driver, 100).until(EC.presence_of_element_located((By.XPATH, "//ytd-transcript-renderer"))).text
+        
+        #remove time stamps from transcript
+        transcript = re.sub(r'[0-9:]', '', transcript)
+        transcript = re.sub(r'\[\s__\s\]', '[__]', transcript)
+
+        return transcript
+
+    def checkProfanity(self, transcript):
+        #check transcript for profanity
+        curse_word_count = 0
+        total_word_count = 0
+        
+        lines = transcript.split('\n')
+        for line in lines:
+            words = line.split(' ')
+            for word in words:
+                if word == '[__]':
+                    curse_word_count += 1
+                total_word_count +=1
+        
+        print(transcript)
+        print('Curse Words: ', curse_word_count, ' Total Words: ', total_word_count)
+                    
 
 
-test = ScraperBot('https://www.youtube.com/watch?v=cZYNADOHhVY')
+
+
+
+test = ScraperBot('https://www.youtube.com/watch?v=_YzD9KW82sk')
 test.createBot()
-test.scrapeTranscript()
+video_transcript = test.scrapeTranscript()
+if video_transcript:
+    test.checkProfanity(video_transcript)
 test.closeBot()
 
 
